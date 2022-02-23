83b6a57a95ad4f51ab10eb9d941dd534ca249677
Felipe27200 <felipe3841@gmail.com>
2022-02-22 21:45:42 -0500

    Constructores, métodos y agregar propiedades

--- Objetos/constructores.js
+++ Objetos/constructores.js
@@ -4,9 +4,14 @@
 // Los parámetros pueden ser las propiedades
 function Persona (nombre, apellido, email){
     // this. establece el parámetro como propiedad
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
+    this.nombre = nombre
+    this.apellido = apellido
+    this.email = email
+
+    // Definir un método para el objeto
+    this.nombreCompleto = function (){
+        return this.nombre + " " + this.apellido;
+    }
 }
 
 // Generar las instancias
@@ -15,3 +20,22 @@ let madre = new Persona("Ana", "Castro", "anCastro@gmail.com")
 
 console.log(padre)
 console.log(madre)
+
+// Llamada al método del objeto
+console.log("Nombre Padre: " + padre.nombreCompleto())
+console.log("Nombre Madre: " + madre.nombreCompleto())
+
+// Agregando instancias a un objeto
+padre.telefono = "333 55 4444"
+
+// padre será el único que tenga la propiedad telefono, madre no por no 
+// hacer referencia a la misma instancia
+console.log(JSON.stringify(padre))
+console.log(JSON.stringify(madre))
+
+// Añadir la propiedad a todos los objetos
+
+Persona.prototype.direccion = "calle 80 #60-95"
+
+console.log(padre.direccion)
+console.log(madre.direccion)
