e02e0d2fe07f18dd91acc95d0cd713507ca79db7
Felipe <felipe3841@gmail.com>
2022-02-25 17:33:04 -0500

    Manejo del Dom

---
+++ DOM/Introduccion/app.js
@@ -0,0 +1,20 @@
+// Así se imprimen valores en al consola
+// del navegador
+console.log('😍❤❤❤😍')
+
+/*
+    Esto muestra la representación del archivo
+    html y una forma superficial de su estructura.
+*/
+console.log(document)
+
+/*
+    Los elementos internos son los objetos
+    hijos, y así es como se accede a ellos,
+    en este caso, se accede al elemento </head>
+*/
+console.log(document.head)
+
+// Así se accede al título del archivo HTML
+// Este es el título que se definio en el head
+console.log(document.title)
---
+++ DOM/eleccionElementos.js/app.js
@@ -0,0 +1,36 @@
+// Obtiene todo el elemento por medio de su id
+let elementoId = document.getElementById('elementoId');
+
+// write() permite escribir en el documento
+document.write("Imprime el objeto que es: " + elementoId);
+
+// Obtener todos los elementos de un mismo tipo
+/*
+    Retorna los objetos como una lista, en este
+    caso obtendríamos todos los elementos </p>
+    del documento.
+*/
+let parrafo = document.getElementsByTagName("p");
+
+/*
+    Se puede acceder a los valores de la lista,
+    mediante un índice, esto no quiere decir,
+    que sea un array
+*/
+document.write("<br><br>Párrafo 1: " + parrafo[0])
+document.write("<br>Párrafo 2: " + parrafo[1])
+
+
+// OBTENER EL PRIMER ELEMENTO CON EL SELECTOR
+/* 
+    Como es una clase y en css estas están precedidas por punto
+    aquí también debe estarlo
+*/
+let querySelector = document.querySelector(".parrafo");
+
+document.write("<br><br>Primer elemento con la clase parrafo: " + querySelector)
+
+// El id de los elementos en css se precedía con #
+let selectorId = document.querySelector("#rancio");
+
+document.write("<br>Primer elemento con el id rancio: " + selectorId)
